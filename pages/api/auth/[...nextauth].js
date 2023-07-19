db1c611915975ca87297a109ad30779351d797ce
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-07-19 17:57:10 -0400

    able to log in and see user login info on mongo DB

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 import clientPromise from "../../../lib/mongodb"
-
+import { MongoDBAdapter } from "@auth/mongodb-adapter" 
 // 1 Here we are setting up the authentication configuration for the application
 // We are using Google as the auth provider and then the adapter Mongo DB, specifies the data storage mechanism for data and session information
 
