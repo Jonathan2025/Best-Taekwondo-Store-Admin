1b73c409918c52b5ecbdb9aaa1197415a0b551d2
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-07-21 17:57:28 -0400

    added states to the fields for change on the new products form

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,10 +1,21 @@
 import Layout from "@/components/Layout";
+import { useState } from "react";
 
 // When the admin clicks the new Product button, they will be led to the new product form page 
 
 export default function NewProduct(){
+
+    // When the fields are changed on the form, we need to hold its value in useState
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [price, setPrice] = useState('')
+ 
+
+
+
     return(
         <Layout>
+            {/* On the form submit we need a function handler */}
             <form onSubmit={}>
 
             
@@ -14,17 +25,19 @@ export default function NewProduct(){
                     type="text"
                     placeholder="New Product"
                     value={title}
-                
+                    // onchange we will change the title state with what was added from the event.target
+                    onChange = {event => setTitle(event.target.value)}
                 />
                 <textarea 
                     placeholder="Description"
                     value={description}
+                    onChange = {event => setDescription(event.target.value)}
                 />
                 <input 
                     type="number" 
                     placeholder="Price"
                     value={price}
-                    
+                    onChange = {event => setPrice(event.target.value)}
                     />
                 <button  
                 type="submit"
