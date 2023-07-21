961e029eac98c49baffca6cd0a18653a86c70667
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-07-20 22:07:02 -0400

    adding the username of the session user and image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 
 export default function Home() {
 
+  const {data: session} = useSession()
+  // capitalize the first letters of each word in the name 
+  const username = session.user.name
+  .split(' ')
+  // using the map function we can apply this to EACH word
+  .map(word => word.charAt(0).toUpperCase() + word.slice(1)) // Pretty much we separate the first letter and the remaining characters and put them back together
+  .join(' ')
+  
   return <Layout>
-    Home Page
+    {/* This will push the introduction to the left side and then the name and image to the right side */}
+    <div className="text-blue-600 flex justify-between">
+     <h2>Hello, <b>{username}</b></h2> 
+      {/* When we look inside the session we can access things like email, name, and image */}
+      <div className="flex bg-gray-200 gap-1 text-black p-2 rounded-2xl">
+        <img src={session.user.image}  className ="w-8 h-8 rounded-2xl"/>
+
+        {username} 
+      </div>
+      
+    </div>
   </Layout>
 }
