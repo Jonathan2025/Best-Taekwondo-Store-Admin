1e7cdf416f3e8157fba3c2a3a79acf7209a69fa7
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-07-25 15:28:31 -0400

    created an upload button for images

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -10,7 +10,8 @@ export default function ProductForm({
     // we dont want the same names as the state names so we use currentTitle, etc
     title: currentTitle,
     description: currentDescription,
-    price: currentPrice
+    price: currentPrice,
+    images
     })
 {
 
@@ -67,6 +68,18 @@ export default function ProductForm({
                     value={description}
                     onChange = {event => setDescription(event.target.value)}
                 />
+                <div className="mt-2 mb-2">
+                    {/* For the upload button center the items and place some space in between */}
+                    <button className="w-32 h-32 text-center flex items-center justify-center gap-1 text-gray-500 rounded-lg bg-gray-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+                        </svg>
+                        Upload
+                    </button>
+                    {!images?.length && (
+                        <div>No Photos in this product</div>
+                    )}
+                </div>
                 <input 
                     type="number" 
                     placeholder="Price"
