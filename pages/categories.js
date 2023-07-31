35111edab0a52eabb3a47dfeebaed6f1ca5ae912
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-07-31 17:05:46 -0400

    creating a new category input

--- pages/categories.js
+++ pages/categories.js
@@ -1,9 +1,30 @@
 import Layout from "@/components/Layout"
 
 const categories = () => {
+
+
+  const [name, saveName] = useState('')
+
+  // Create a save category handler function that will run on submit 
+  const saveCategory = () => {
+
+  }
   return (
     <Layout>
         <h1>Categories</h1>
+
+         {/* flex used to put input category name NEXT to a save button */}
+        <form onSubmit={saveCategory} className="flex gap-1"> 
+        {/* remove the slight margin at the bottom */}
+          <input 
+            className="mb-0" 
+            type="text" 
+            placeholder={'Category Name'}
+            // set the name to event.target when the input is changed
+            onChange = {event => setName(event.target.value)} 
+            />  
+          <button type="submit" className="btn-primary py-1">Save</button>
+        </form>
         
     </Layout>
   )
