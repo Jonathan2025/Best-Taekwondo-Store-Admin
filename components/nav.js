c7c764e6da0508e0069950a8f5386606ac8a7bfc
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-08-08 14:43:24 -0400

    added a signout button which uses next-auth

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,7 @@
 // Here we created a navigation component that will be called in the index.js
 import Link from "next/link"
 import { useRouter } from "next/router"
+import { signOut } from "next-auth/react"
 
 export default function NavBar() {
 
@@ -67,6 +68,14 @@ export default function NavBar() {
                 </svg>
                 Settings
             </Link>
+
+            <button onClick = {() => signOut()} className={pathname.includes('/settings') ? activeLink : inactiveLink}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                </svg>
+
+                Logout
+            </button>
 
             </nav>
         </aside>
