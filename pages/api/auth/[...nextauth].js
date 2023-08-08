0068e92eb2ea37153ac29c84b879b793fc3af625
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-08-08 14:54:31 -0400

    able to add a callback to only allow certain admin accounts to access

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,6 +5,9 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter"
 // 1 Here we are setting up the authentication configuration for the application
 // We are using Google as the auth provider and then the adapter Mongo DB, specifies the data storage mechanism for data and session information
 
+const adminEmails = [process.env.ADMIN_EMAILS]
+
+
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
@@ -14,4 +17,17 @@ export default NextAuth({
     }),
   ], 
   adapter: MongoDBAdapter(clientPromise),
+
+  // Callbacjs are async functions that can be used to control what happens when an action is performed 
+  // So in this case we can implement to allow certain users to be able to access 
+  callbacks: {
+    session: ({session, token, user}) => {
+
+      if(adminEmails.includes(session?.user?.email)){
+        return session
+      } else {
+        return false 
+      }
+    }
+  }
 })
