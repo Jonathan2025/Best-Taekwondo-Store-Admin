39b36b1a20b4d3f9827a02f21aa1ef00b8178faa
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-09-01 12:02:43 -0400

    best taekwondo logo and edited welcome message

--- components/Layout.js
+++ components/Layout.js
@@ -33,13 +33,13 @@ export default function Layout({children}) {
       <div className = "bg-sky-600 min-h-screen"> 
         {/* // Otherwise if we are logged in then show the user. Minimum height to be screen */}
         {/* when we are on a medium sized screen then we will make the hamburger hidden */}
-        <div className = "block md:hidden flex items-center p-"> 
+        <div className = "block md:hidden flex items-center"> 
           <button onClick={()=> setShowNav(true)}>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5" />
             </svg>
           </button>
-          {/* have the logo take up the entire widthe with flex grow and then center it  */}
+          {/* have the logo take up the entire width with flex grow and then center it  */}
           <div className = "flex grow justify-center">
             <Logo />
           </div>
--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,10 @@ export default function Home() {
   return <Layout>
     {/* This will push the introduction to the left side and then the name and image to the right side */}
     <div className="text-blue-600 flex justify-between">
-     <h2>Hello, <b>{username}</b></h2> 
+      <h2>
+        Hello <b>{username}</b>! <br /> Welcome to the Best Taekwondo Online Admin Panel
+      </h2>
+    
       {/* When we look inside the session we can access things like email, name, and image */}
       <div className="flex bg-gray-200 gap-1 text-black p-2 rounded-2xl">
         <img src={session?.user?.image}  className ="w-8 h-8 rounded-2xl"/>
