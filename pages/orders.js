df41d326b0d38e8783ec6a85c0dba237d742974a
jonathan2025 <Jonathan.Liang75@outlook.com>
2023-09-07 14:15:36 -0400

    added in the orders model and planning backbone of the orders admin page

---
+++ pages/orders.js
@@ -0,0 +1,15 @@
+import Layout from "@/components/Layout"
+
+// Here for the orders page we will build out a table with all the orders information 
+const OrdersPage = () => {
+  return (
+    <Layout>
+        <h1>Orders</h1>
+        <table>
+
+        </table>
+    </Layout>
+  )
+}
+
+export default OrdersPage
